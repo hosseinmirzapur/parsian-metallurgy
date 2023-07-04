7be350b16224be5c964ed79c75e58b1b86f6e773
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-07-05 00:16:28 +0330

    updated home page metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 import Hero from "./components/sections/Hero"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+	title: "متالورژی پارسیان",
+	description: "این وبسایت جهت تست دمو و تایید دیزاین اولیه است",
+}
 
 export default function Home() {
 	return (
