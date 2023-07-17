2d377800a415711bc7abb3ea4033fb9c81a1c730
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-07-17 04:14:39 +0330

    beautify FAQ section

--- app/components/builders/Container.tsx
+++ app/components/builders/Container.tsx
@@ -7,9 +7,7 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = ({ children, className }) => {
 	return (
-		<div
-			className={`w-11/12 md:w-10/12 lg:w-9/12 mx-auto ${className}`}
-			dir="rtl">
+		<div className={`w-11/12 mx-auto ${className}`} dir="rtl">
 			{children}
 		</div>
 	)
--- app/faq/FaqContainer.tsx
+++ app/faq/FaqContainer.tsx
@@ -8,6 +8,8 @@ import {
 import Container from "../components/builders/Container"
 import { useState } from "react"
 import { anchor } from "../utils/css"
+import Image from "next/image"
+import FaqImage from "../../public/faq.svg"
 
 const faqItems: AccordionItem[] = [
 	{
@@ -39,38 +41,39 @@ const FaqContainer = () => {
 		<Container className="flex flex-col gap-14 pt-10 pb-20">
 			<h1 className="text-p-black text-center text-2xl">سوالات متداول</h1>
 
-			<Container className="from-gray-800 to-black bg-gradient-to-t rounded-xl">
-				{faqItems.map((faq, index) => (
-					<Accordion
-						open={open === index}
-						id={index.toString()}
-						className="flex flex-col"
-						key={index}
-						dir="rtl">
-						<AccordionHeader
-							dir="rtl"
-							onClick={() => handleOpen(index)}
-							className={`
+			<div className="flex flex-col lg:flex-row gap-5 justify-between">
+				<Container className="from-gray-800 to-black bg-gradient-to-t rounded-xl">
+					{faqItems.map((faq, index) => (
+						<Accordion
+							open={open === index}
+							id={index.toString()}
+							className="flex flex-col"
+							key={index}
+							dir="rtl">
+							<AccordionHeader
+								dir="rtl"
+								onClick={() => handleOpen(index)}
+								className={`
                                 text-p-white
                                 text-[12px]
                                 md:text-[13px]
-                                lg:text-[14px]
+                                lg:text-[16px]
                                 ${anchor().styles}
                                 py-4
                                 px-1
-                                md:p-2
-                                lg:p-3
                             `}>
-							{faq.question}
-						</AccordionHeader>
-						<AccordionBody>
-							<h3 className="text-p-white text-base p-2 font-sans">
-								{faq.answer}
-							</h3>
-						</AccordionBody>
-					</Accordion>
-				))}
-			</Container>
+								{faq.question}
+							</AccordionHeader>
+							<AccordionBody>
+								<h3 className="text-p-white text-base p-2 font-sans">
+									{faq.answer}
+								</h3>
+							</AccordionBody>
+						</Accordion>
+					))}
+				</Container>
+				<Image alt="faq-image" src={FaqImage} />
+			</div>
 		</Container>
 	)
 }
